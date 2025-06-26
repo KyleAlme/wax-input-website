b8a8689a303cd61d1d076d8dfc726b184966c8af
KyleAlme <almeidakyle03@gmail.com>
2025-06-26 17:21:35 -0400

    WIP: get links in header nav working

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,16 +1,32 @@
 import './index.css'
+import { Link } from 'react-router-dom'
 
 function CustomHeader() {
-
   return (
     <div>
       <header className="sticky top-0 text-white shadow-md">
         <nav className="container mx-auto flex justify-center py-5">
           <ul className="flex space-x-8 text-xl sm:space-x-20">
-            <li><a href="#" className="hover:text-red-600 inline-block transform hover:scale-125 transition-transform">Home</a></li>
-            <li><a href="#" className="hover:text-red-600 inline-block transform hover:scale-125 transition-transform">Gallery</a></li>
-            <li><a href="#" className="hover:text-red-600 inline-block transform hover:scale-125 transition-transform">Shows</a></li>
-            <li><a href="#" className="hover:text-red-600 inline-block transform hover:scale-125 transition-transform">Merch</a></li>
+            <li>
+              <Link to="/" className="hover:text-red-600 inline-block transform hover:scale-125 transition-transform">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/gallery" className="hover:text-red-600 inline-block transform hover:scale-125 transition-transform">
+                Gallery
+              </Link>
+            </li>
+            <li>
+              <Link to="/shows" className="hover:text-red-600 inline-block transform hover:scale-125 transition-transform">
+                Shows
+              </Link>
+            </li>
+            <li>
+              <Link to="/merch" className="hover:text-red-600 inline-block transform hover:scale-125 transition-transform">
+                Merch
+              </Link>
+            </li>
           </ul>
         </nav>
       </header>
@@ -19,3 +35,4 @@ function CustomHeader() {
 }
 
 export default CustomHeader
+
---
+++ src/pages/merch.jsx
@@ -0,0 +1,14 @@
+import '../index.css';
+import CustomHeader from '../Header.jsx';
+
+function MerchPage() {
+
+    return (
+        <>
+            <p className="text-white text-xl">Merch Page</p>
+        </>
+    )
+
+}
+
+export default MerchPage;
