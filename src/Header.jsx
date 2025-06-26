ba0f11a0a773f56fe4a1779a769051fbcc254ce6
KyleAlme <almeidakyle03@gmail.com>
2025-06-26 15:34:34 -0400

    fix: Change header spacing on different screen sizes

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,7 +6,7 @@ function CustomHeader() {
     <div>
       <header className="sticky top-0 text-white shadow-md">
         <nav className="container mx-auto flex justify-center py-5">
-          <ul className="flex space-x-12 text-xl">
+          <ul className="flex space-x-8 text-xl sm:space-x-20">
             <li><a href="#" className="hover:text-red-600 inline-block transform hover:scale-125 transition-transform">Home</a></li>
             <li><a href="#" className="hover:text-red-600 inline-block transform hover:scale-125 transition-transform">Gallery</a></li>
             <li><a href="#" className="hover:text-red-600 inline-block transform hover:scale-125 transition-transform">Shows</a></li>
