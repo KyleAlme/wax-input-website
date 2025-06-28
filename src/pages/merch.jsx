23979f3e25745210cf6c950621e27cfcbb84c07a
KyleAlme <almeidakyle03@gmail.com>
2025-06-28 18:18:21 -0400

    feat: Add Merch page

--- src/pages/merch.jsx
+++ src/pages/merch.jsx
@@ -9,13 +9,13 @@ function MerchPage() {
             <p className="text-white text-2xl text-center">The Merchandise section is not completed/active (and there is currently no timetable to get it running).
                 Please enjoy these funny photos of the band instead.
             </p>
-            <div className="flex max-h-[75vh] w-[70%] mx-auto mt-4 mb-4">
+            <div className="flex max-h-[75vh] w-[70%] mx-auto mt-8 mb-8">
                 <img src="BandPhoto2.jpg" alt="The Band at Halloween"className="object-contain w-full"></img>
             </div>
-            <div className="flex max-h-[75vh] w-[70%] mx-auto mt-4 mb-4">
+            <div className="flex max-h-[75vh] w-[70%] mx-auto mt-8 mb-8">
                 <img src="DitAtWar.jpg" alt="Dit at War"className="object-contain w-full"></img>
             </div>
-            <div className="flex max-h-[75vh] w-[70%] mx-auto mt-4 mb-4">
+            <div className="flex max-h-[75vh] w-[70%] mx-auto mt-8 mb-8">
                 <img src="DitSnap.jpg" alt="Dit Pic"className="object-contain w-full"></img>
             </div>
         </>
