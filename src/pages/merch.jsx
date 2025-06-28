a3d9a4a946cbb441ab8ea8271126ecab8c421462
KyleAlme <almeidakyle03@gmail.com>
2025-06-28 17:55:40 -0400

    WIP: Add/move around pictures on merch and home

--- src/pages/merch.jsx
+++ src/pages/merch.jsx
@@ -5,7 +5,19 @@ function MerchPage() {
 
     return (
         <>
-            <p className="text-white text-xl">Merch Page</p>
+            <p className="text-white text-4xl text-center py-4">Sorry!</p>
+            <p className="text-white text-2xl text-center">The Merchandise section is not completed/active (and there is currently no timetable to get it running).
+                Please enjoy these funny photos of the band instead.
+            </p>
+            <div className="flex max-h-[75vh] w-[70%] mx-auto mt-4 mb-4">
+                <img src="BandPhoto2.jpg" alt="The Band at Halloween"className="object-contain w-full"></img>
+            </div>
+            <div className="flex max-h-[75vh] w-[70%] mx-auto mt-4 mb-4">
+                <img src="DitAtWar.jpg" alt="Dit at War"className="object-contain w-full"></img>
+            </div>
+            <div className="flex max-h-[75vh] w-[70%] mx-auto mt-4 mb-4">
+                <img src="DitSnap.jpg" alt="Dit Pic"className="object-contain w-full"></img>
+            </div>
         </>
     )
 
