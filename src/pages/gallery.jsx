dbe16ae695b9082d73fe32ef4e01d36e262af174
KyleAlme <almeidakyle03@gmail.com>
2025-07-08 17:51:02 -0400

    feat: Finish gallery page

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -54,6 +54,27 @@ function Gallery() {
             </div>
         </div>         
 
+        <div className="h-[10vh] flex flex-col bg-white w-full mt-5">
+            <p className="bg-inherit text-black font-black text-lg text-center">Where To Find Wax Input:</p>
+            <div className="bg-inherit flex justify-center gap-x-8 py-2">
+                <a href="https://open.spotify.com/artist/6mUhaPUPQGzNWuwxHZKEpH">
+                    <img src="SpotifyLogo.png" className="h-8 object-contain bg-white"></img>
+                </a>
+                <a href="https://soundcloud.com/wax-input">
+                    <img src="SoundcloudLogo.svg" className="h-8 object-contain bg-white"></img>
+                </a>
+                <a href="https://www.instagram.com/wax.input/">
+                    <img src="InstagramLogo.svg" className="h-8 object-contain bg-white"></img>
+                </a>
+                <a href="https://music.apple.com/us/artist/wax-input/1705341034">
+                    <img src="AppleLogo.svg" className="h-8 object-contain bg-white"></img>
+                </a>
+                <a href="https://www.youtube.com/@WaxInput">
+                    <img src="YoutubeLogo.svg" className="h-8 object-contain bg-white"></img>
+                </a>
+            </div>
+        </div>
+
         </>
     );
 
