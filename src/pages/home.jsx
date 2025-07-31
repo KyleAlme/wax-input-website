ae730554d20d55bbc4b47f8f7226703ee993082d
KyleAlme <almeidakyle03@gmail.com>
2025-07-31 15:16:52 -0400

    feat: add new slideshow

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -74,11 +74,11 @@ function HomePage() {
             </div>
         </div>
 
-        <div className='flex w-full justify-center mb-12 mt-4'>
+        <div className='flex w-full justify-center mt-4'>
             <a href="https://open.spotify.com/album/4nbcRfNBeQTBxbJbsQKvsI?si=c0U0lyQaTV-ytwHYikVFqw" className='text-white underline text-2xl md:text-4xl'>Listen now!</a>
         </div>
 
-        <div className="flex flex-col w-full items-center text-white mt-5" id="show-section">
+        <div className="flex flex-col w-full items-center text-white mt-16" id="show-section">
             <p className="font-extrabold text-4xl mb-2 md:mt-5 md:text-5xl">Upcoming Shows</p>
 
             <div className="w-full border-t-2 border-b-2 flex">
